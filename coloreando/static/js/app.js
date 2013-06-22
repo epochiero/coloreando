511a6a99578e4806235f03dc90bdbb4739c04ab8
Tim Zenderman <tzenderman@gmail.com>
2013-06-22 16:06:11 -0300

    added css for dashboard

---
+++ coloreando/static/js/app.js
@@ -0,0 +1,25 @@
+var stage, shape, oldX, oldY, size;
+
+$(function() {
+  stage = new createjs.Stage("dashboard-canvas");
+  stage.enableDOMEvents = true;
+
+  shape = new createjs.Shape();
+  stage.addChild(shape);
+
+  size = 3;
+
+  stage.addEventListener('stagemousemove', function(e) {
+    if (oldX) {
+      shape.graphics.beginStroke(color)
+                    .setStrokeStyle(size, "round")
+                    .moveTo(oldX, oldY)
+                    .lineTo(e.stageX, e.stageY);
+      stage.update();
+    }
+    oldX = e.stageX;
+    oldY = e.stageY;
+  });
+
+  stage.update();
+});
