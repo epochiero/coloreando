efc0eaeecece1121d43e91b184e7df7a0bc67225
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-06-23 02:49:02 -0300

    Actualizando la lista de usuarios al ingresar a la aplicación.

--- coloreando/static/js/coloreando.js
+++ coloreando/static/js/coloreando.js
@@ -1,9 +1,18 @@
 var coloreando = angular.module('coloreando', []);
 
 
-coloreando.controller('BuddiesController', function($scope) {
-  $scope.buddies = [
-    {username: 'example', color_id: 'red'},
-  ];
+coloreando.controller('BuddiesController', function($scope, $http) {
+
+  $scope.buddies = [];
+
+  $.get('http://127.0.0.1:8000/api/getBuddies?dashboard_id=' + dashboard_id, function(data) {
+    $scope.buddies = data.buddies;
+    $scope.$apply();
+  });
+
+  // Solicita los usuarios iniciales a la aplicacion.
+  $scope.add_buddy = function(user, color) {
+    $scope.buddies.push({username: user, color_id: color});
+  }
 
 });
