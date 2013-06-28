4a350457aed37f82ed781554422fb7ae01bbffb5
Ezequiel Pochiero <epochiero@gmail.com>
2013-06-28 17:30:26 -0300

    fix hardcoded url

--- coloreando/static/js/coloreando.js
+++ coloreando/static/js/coloreando.js
@@ -5,7 +5,7 @@ coloreando.controller('BuddiesController', function($scope, $http) {
 
   $scope.buddies = [];
 
-  $.get('http://127.0.0.1:8000/api/getBuddies?dashboard_id=' + dashboard_id, function(data) {
+  $.get('/api/getBuddies?dashboard_id=' + dashboard_id, function(data) {
     $scope.buddies = data.buddies;
     $scope.$apply();
   });
